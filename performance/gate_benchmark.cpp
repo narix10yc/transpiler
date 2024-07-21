9748c5d838e67ef6b643ea3b57e78c606f79785c
yl5619 <yl5619@ic.ac.uk>
2024-07-21 16:53:57 +0100

    update gate benchmark

--- performance/gate_benchmark.cpp
+++ performance/gate_benchmark.cpp
@@ -1,21 +1,33 @@
 #include "gen_file.h"
 #include "utils/statevector.h"
+#include "utils/iocolor.h"
 #include "timeit/timeit.h"
 #include <iomanip>
+#include <fstream>
 
 #ifdef USING_F32
     using Statevector = utils::statevector::StatevectorSep<float>;
 #else 
     using Statevector = utils::statevector::StatevectorSep<double>;
 #endif
+
 using namespace timeit;
+using namespace Color;
 
 int main(int argc, char** argv) {
-    Statevector sv(24);
+    Statevector sv(30);
 
     Timer timer;
-    timer.setReplication(5);
+    timer.setReplication(15);
     TimingResult rst;
+
+    if (argc <= 1) {
+        std::cerr << RED_FG << "Error: " << RESET << "Need to provide output file\n";
+        return 1;
+    }
+    std::cerr << "-- Output file: " << argv[1] << "\n";
+
+    std::ofstream file(argv[1]);
 
     #ifdef MULTI_THREAD_SIMULATION_KERNEL
     std::cerr << "Multi-threading enabled.\n";
@@ -24,16 +36,18 @@ int main(int argc, char** argv) {
     // const std::vector<int> nthreadsVec {16,24,32,36,48,64,68,72};
     // const std::vector<int> nthreadsVec {16,20,24,28,32,34,36};
 
-    const std::vector<int> nthreadsVec {2,4};
+    const std::vector<int> nthreadsVec {8, 16};
 
-    for (unsigned i = 0; i < nthreadsVec.size(); i++) {
-        int nthreads = nthreadsVec[i];
-        std::cerr << nthreads << "-thread:\n";
-        uint64_t idxMax;
-        uint64_t chunkSize;
-        std::vector<std::thread> threads(nthreads);
-        for (const auto& data : _metaData) {
-            std::cerr << "OpCount " << data.opCount << "; nqubits " << data.nqubits << ": time = ";
+    uint64_t idxMax;
+    uint64_t chunkSize;
+    for (const auto& data : _metaData) {
+        if ((1ULL << static_cast<int>(std::log2(data.opCount))) != data.opCount)
+            continue;
+        std::cerr << "OpCount " << data.opCount << "; nqubits " << data.nqubits << "\n";
+        file << data.opCount << ",";
+        for (unsigned i = 0; i < nthreadsVec.size(); i++) {
+            int nthreads = nthreadsVec[i];
+            std::vector<std::thread> threads(nthreads);
             rst = timer.timeit(
                 [&]() {
                         idxMax = 1ULL << (sv.nqubits - data.nqubits - S_VALUE);
@@ -43,8 +57,13 @@ int main(int argc, char** argv) {
                         for (unsigned i = 0; i < nthreads; i++)
                             threads[i].join();
                     });
-            std::cerr << rst.timeToString(rst.min, 4) << "\n";
+            std::cerr << rst.timeToString(rst.min, 4) << " ";
+            file << std::scientific << std::setprecision(4) << rst.min;
+            if (i < nthreadsVec.size() - 1)
+                file << ",";
         }
+        file << "\n";
+        std::cerr << "\n";
 
     }
 
