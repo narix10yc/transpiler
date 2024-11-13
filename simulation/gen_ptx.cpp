79a421c0b2ac3a041630d3f1ed8ab4c70613d773
yc <unknown>
2024-11-13 02:02:25 +0000

    updated a version of ptx gen

--- simulation/gen_ptx.cpp
+++ simulation/gen_ptx.cpp
@@ -94,7 +94,9 @@ Function* IRGenerator::generateCUDAKernel(
     idxStartV = builder.CreateAdd(idxStartV, tmpCounterV, "idxStart");
 
     uint64_t N = 1 << nqubits;
-    std::vector<Value*> reMats(N), imMats(N), reAmpPtrs(N), imAmpPtrs(N), reAmps(N), imAmps(N);
+    // std::vector<Value*> reMats(N), imMats(N), reAmpPtrs(N), imAmpPtrs(N), reAmps(N), imAmps(N);
+    std::vector<Value*> reAmpPtrs(N), imAmpPtrs(N), reAmps(N), imAmps(N);
+
     for (uint64_t i = 0; i < N; i++) {
         uint64_t delta = 0ULL;
         for (int b = 0; b < nqubits; b++) {
@@ -111,14 +113,14 @@ Function* IRGenerator::generateCUDAKernel(
     }
 
     for (int r = 0; r < N; r++) {
-        for (int c = 0; c < N; c++) {
-            // load the r-th row of the matrix
-            std::string suffix = std::to_string(r) + "." + std::to_string(c);
-            auto* idxReV = builder.CreateConstGEP1_64(scalarTy, pMatArg, 2ULL * (N*r + c), "idx.mat.re." + suffix);
-            reMats[c] = builder.CreateLoad(scalarTy, idxReV, "mat.re." + suffix);
-            auto* idxImV = builder.CreateConstGEP1_64(scalarTy, pMatArg, 2ULL * (N*r + c) + 1, "idx.mat.im." + suffix);
-            imMats[c] = builder.CreateLoad(scalarTy, idxImV, "mat.im." + suffix);
-        }
+        // for (int c = 0; c < N; c++) {
+        //     // load the r-th row of the matrix
+        //     std::string suffix = std::to_string(r) + "." + std::to_string(c);
+        //     auto* idxReV = builder.CreateConstGEP1_64(scalarTy, pMatArg, 2ULL * (N*r + c), "idx.mat.re." + suffix);
+        //     reMats[c] = builder.CreateLoad(scalarTy, idxReV, "mat.re." + suffix);
+        //     auto* idxImV = builder.CreateConstGEP1_64(scalarTy, pMatArg, 2ULL * (N*r + c) + 1, "idx.mat.im." + suffix);
+        //     imMats[c] = builder.CreateLoad(scalarTy, idxImV, "mat.im." + suffix);
+        // }
 
         // matrix-vector multiplication
         // Value* updatedReAmp = reAmps[r];
@@ -132,9 +134,40 @@ Function* IRGenerator::generateCUDAKernel(
         // builder.CreateStore(updatedReAmp, reAmpPtrs[r]);
         // builder.CreateStore(updatedImAmp, imAmpPtrs[r]);
 
-        auto updated = genComplexDotProduct(reAmps, imAmps, reMats, imMats);
-        builder.CreateStore(updated.first, reAmpPtrs[r]);
-        builder.CreateStore(updated.second, imAmpPtrs[r]);
+        // mat-vec mul alternative version
+        // auto updated = genComplexDotProduct(reAmps, imAmps, reMats, imMats);
+        // builder.CreateStore(updated.first, reAmpPtrs[r]);
+        // builder.CreateStore(updated.second, imAmpPtrs[r]);
+
+        // Alternative version, potentially having better locality
+        // updatedReAmp = sum(reAmps_i * reMats_i) - sum(imAmps_i * imMats_i)
+        // updatedImAmp = sum(reAmps_i * imMats_i) + sum(imAmps_i * reMats_i)
+        Value* reMatPtr = builder.CreateConstGEP1_64(scalarTy, pMatArg, 0ULL, "mat.re.ptr." + std::to_string(r) + ".0");
+        Value* imMatPtr = builder.CreateConstGEP1_64(scalarTy, pMatArg, 1ULL, "mat.im.ptr." + std::to_string(r) + ".0");
+        Value* reMat = builder.CreateLoad(scalarTy, reMatPtr, "mat.re." + std::to_string(r) + ".0");
+        Value* imMat = builder.CreateLoad(scalarTy, imMatPtr, "mat.im." + std::to_string(r) + ".0");
+        Value* updatedReAmp0 = builder.CreateFMul(reAmps[0], reMat);
+        Value* updatedReAmp1 = builder.CreateFMul(imAmps[0], imMat);
+        Value* updatedImAmp = builder.CreateFMul(reAmps[0], imMat);
+        for (int c = 1; c < N; c++) {
+            std::string suffix = std::to_string(r) + "." + std::to_string(c);
+            reMatPtr = builder.CreateConstGEP1_64(scalarTy, pMatArg, 2ULL * (N*r + c), "idx.mat.re." + suffix);
+            reMat = builder.CreateLoad(scalarTy, reMatPtr, "mat.re." + suffix);
+            imMatPtr = builder.CreateConstGEP1_64(scalarTy, pMatArg, 2ULL * (N*r + c) + 1, "idx.mat.im." + suffix);
+            reMat = builder.CreateLoad(scalarTy, imMatPtr, "mat.im." + suffix);
+
+            updatedReAmp0 = builder.CreateIntrinsic(
+                scalarTy, Intrinsic::fmuladd, { reAmps[c], reMat, updatedReAmp0 });
+            updatedReAmp1 = builder.CreateIntrinsic(
+                scalarTy, Intrinsic::fmuladd, { imAmps[c], imMat, updatedReAmp1 });
+            updatedImAmp = builder.CreateIntrinsic(
+                scalarTy, Intrinsic::fmuladd, { reAmps[c], imMat, updatedImAmp });
+            updatedImAmp = builder.CreateIntrinsic(
+                scalarTy, Intrinsic::fmuladd, { imAmps[c], reMat, updatedImAmp });
+        }
+        Value* updatedReAmp = builder.CreateFSub(updatedReAmp0, updatedReAmp1);
+        builder.CreateStore(updatedReAmp, reAmpPtrs[r]);
+        builder.CreateStore(updatedImAmp, imAmpPtrs[r]);
     }
 
     builder.CreateRetVoid();
