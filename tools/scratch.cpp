101ec381b198f033c9d2dc738efaddc6b1de43b0
yl5619 <yl5619@ic.ac.uk>
2025-03-05 00:15:04 +0000

    added a demo folder

--- tools/scratch.cpp
+++ tools/scratch.cpp
@@ -1,31 +1,3 @@
-#include "cast/Parser.h"
-#include "cast/CircuitGraph.h"
-#include "simulation/KernelManager.h"
-
-using namespace cast;
-
-int main(int argc, char** argv) {
-  assert(argc > 1);
-  Parser parser(argv[1]);
-  auto qc = parser.parseQuantumCircuit();
-  CircuitGraph graph;
-  qc.toCircuitGraph(graph);
-
-  CUDAKernelManager cudaKernelMgr;
-  CUDAKernelGenConfig cudaGenConfig;
-  cudaGenConfig.displayInfo(std::cerr) << "\n";
-
-  utils::timedExecute([&]() {
-    cudaKernelMgr.genCUDAGatesFromCircuitGraph(cudaGenConfig, graph, "myGraph");
-  }, "CUDA Kernel Generation");
-
-  utils::timedExecute([&]() {
-    cudaKernelMgr.emitPTX(2, llvm::OptimizationLevel::O1, /* verbose */ 1);
-  }, "PTX Code Emission");
-
-  utils::timedExecute([&]() {
-    cudaKernelMgr.initCUJIT(3, /* verbose */ 1);
-  }, "CUDA JIT Initialization");
-
+int main() {
   return 0;
 }
